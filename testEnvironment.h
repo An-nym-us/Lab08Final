5d62fd9abec0bad89f292ae5401720d5a2276e5b
Its-suLav-D <dah19001@byui.edu>
2022-03-04 14:39:10 -0700

    final  interpolation changes

--- testEnvironment.h
+++ testEnvironment.h
@@ -16,6 +16,12 @@ public:
 	{
 		get_zero_alt();
         test_same_point();
+        test_middle_x_change();
+        test_middle_both_change();
+        test_forward_one_side();
+        test_forward_other_side();
+        test_backward_one_side();
+        test_backward_other_side();
 
 	}
 
@@ -53,7 +59,7 @@ public:
         
         bool value = false;
         // exercise
-        if(envInstance.interpolation_check_same_point(0.0, 0.0, 0.0, 0.0, 0.0) == 0.0)
+        if(envInstance.interpolation_check(0.0, 0.0, 0.0, 0.0, 0.0) == 0.0)
             value = true;
         
         // verify
@@ -63,5 +69,128 @@ public:
         delete& envInstance;
     
     }
+    
+    void test_middle_x_change()
+    {
+        // set up
+        
+        Environment envInstance;
+        
+        bool value = false;
+        
+        // exercise
+        
+        if (envInstance.interpolation_check(0.0, 0.0, 2.0, 0.0, 1.00) == 0.00)
+            value = true;
+        
+        // verify
+        assert(value == true);
+        
+        // teardown
+        
+        delete& envInstance;
+    }
+    
+    
+    void test_middle_both_change()
+    {
+        // setup
+        
+        Environment envInstance;
+        bool value = false;
+        
+        // exercise
+        
+        if(envInstance.interpolation_check(0.0, 0.0, 2.0, 2.0, 1.00) == 1.00)
+            value = true;
+        
+        // verify
+        assert(value == true);
+        
+        // teardown
+        
+        delete& envInstance;
+    }
+    
+    void test_forward_one_side()
+    {
+        // setup
+        
+        Environment envInstance;
+        bool value = false;
+        
+        // exercise
+        
+        if(envInstance.interpolation_check(1.0, 2.0, 2.0, 3.0, 1.25) == 2.25)
+            value = true;
+        
+        // verify
+        assert(value == true);
+        
+        // teardown
+        
+        delete& envInstance;
+    }
+    
+    void test_forward_other_side()
+    {
+        // setup
+        
+        Environment envInstance;
+        bool value = false;
+        
+        // exercise
+        
+        if(envInstance.interpolation_check(1.0, 2.0, 2.0, 3.0, 1.75) == 2.75)
+            value = true;
+        
+        // verify
+        assert(value == true);
+        
+        // teardown
+        
+        delete& envInstance;
+    }
+    
+    void test_backward_one_side()
+    {
+        // setup
+        
+        Environment envInstance;
+        bool value = false;
+        
+        // exercise
+        
+        if(envInstance.interpolation_check(2.0, 2.0, 1.0, 3.0, 1.50) == 2.50)
+            value = true;
+        
+        // verify
+        assert(value == true);
+        
+        // teardown
+        
+        delete& envInstance;
+    }
+    
+    void test_backward_other_side()
+    {
+        // setup
+        
+        Environment envInstance;
+        bool value = false;
+        
+        // exercise
+        
+        if(envInstance.interpolation_check(2.0, 2.0, 1.0, 3.0, 1.75) == 2.25)
+            value = true;
+        
+        // verify
+        assert(value == true);
+        
+        // teardown
+        
+        delete& envInstance;
+    }
+    
 
 };
